26c7f603e40893cba67744acd52b9eccd29cb69f
RickdoMascoMAT <riccardo.mascotto@mat.tn.it>
2025-12-13 16:23:44 +0100

    style: update hero section styles for improved layout and responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom'
 import logo from '../assets/erasebg-transformed-downscaled.png'
 
 export default function Hero() {
@@ -7,7 +6,6 @@ export default function Hero() {
       <img src={logo} className="hero-logo" alt="IronRhino Logo" />
       <h1>Benvenuti in IronRhino</h1>
       <p>Promuovi uno stile di vita sano attraverso lo sport e preparati per le gare.</p>
-      <Link to="/abbonamenti" className="btn">Inizia Ora - Prova Gratuita</Link>
     </section>
   )
 }
